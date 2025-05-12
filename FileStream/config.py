c802dc6c55388a41793b4cfa1c3c04be85529ebd
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2025-05-12 21:14:40 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -12,8 +12,8 @@ class Telegram:
     DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://leechbot:jodgod@leechbot.23hzrrg.mongodb.net/?retryWrites=true&w=majority&appName=leechbot"))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Drxupdate"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', Drxupdate)
+    FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1002391879845")
+    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', "Drxupdate")
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
